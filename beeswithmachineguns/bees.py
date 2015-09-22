902a2e826b1a862fdd4277ce82023eff8f2f62a1
Chris Kolosiwsky <ckolos@gmail.com>
2015-09-22 14:22:54 -0500

    set fs.file-max to a stupidly high number

--- beeswithmachineguns/bees.py
+++ beeswithmachineguns/bees.py
@@ -124,6 +124,7 @@ sysctl -w net.core.rmem_max='16777216';
 sysctl -w net.core.wmem_max='16777216';
 sysctl -w net.ipv4.tcp_rmem='4096 87380 16777216';
 sysctl -w net.ipv4.tcp_wmem='4096 65536 16777216';
+sysctl -w fs.file-max = '1048576000';
 
 
 echo 'installing stuff'"""
